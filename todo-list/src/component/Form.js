bbb21509a47025d6fa95b81076f848b022998f16
Fani Gouera <81867015+fani-g@users.noreply.github.com>
2024-01-23 19:08:33 +0200

    fixing style

--- todo-list/src/component/Form.js
+++ todo-list/src/component/Form.js
@@ -4,7 +4,7 @@ const Form = (props) => {
         <div className="row justify-content-center">
             <div className="col-6">
                 <form onSubmit={props.handleSubmit} className="m-4">
-                    <div class="input-group mb-3">
+                    <div className="input-group mb-3">
                         <input className="form-control" value={props.newTask} onChange={props.handleChange} />
                         <button type="submit" className="btn btn-primary mx-2" onClick={props.addTask}><i className="bi bi-plus-lg"></i></button>
                     </div>
--- todo-list/src/component/Task.js
+++ todo-list/src/component/Task.js
@@ -1,11 +1,11 @@
 const Task = (props) => {
     return (
         <div className="row justify-content-center align-items-center m-2">
-            <div className="col-4 bg-light rounded">
-                <p className="text-center">{props.taskname}</p>     
+            <div className="col-4 bg-light rounded text-center p-2">
+                {props.taskname}   
             </div>
             <div className="col-1">
-                <div class="btn-group justify-content-start" role="group">
+                <div className="btn-group" role="group">
                     <button className="btn btn-success text-white" onClick={()=> props.editTask(props.id)}><i className="bi bi-pencil-square"></i></button>
                     <button className="btn btn-danger text-white" onClick={()=> props.deleteTask(props.id)}><i className="bi bi-trash"></i></button>  
                 </div>
